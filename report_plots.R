01deac4a4fb12f0782b2efc9a8f41bb8f348661a
Arni Magnusson <arnima@hafro.is>
2017-10-03 20:48:55 +0200

    Use ido()

--- report_plots.R
+++ report_plots.R
@@ -23,23 +23,23 @@ abline(h=45, lwd=3, col="orange")
 abline(h=45, lwd=2, lty=2)
 legend("topleft", c("Btrigger","Blim"), lwd=3:2, lty=1:2,
        col=c("orange","black"), bty="n")
-invisible(dev.off())
+ido()
 
 tafpng("harvest_rate")
 plot(HR~Year, x, subset=Year<=2014, type="l", lwd=2, ylim=ylim(x$HR), yaxs="i",
      ylab="Harvest rate", main="Harvest rate")
 abline(h=c(0.40), lty=2, lwd=2)
 legend("bottomleft", "HRtarget", lty=2, lwd=2, bty="n")
-invisible(dev.off())
+ido()
 
 tafpng("landings")
 barplot(x$Landings[x$Year<=2014], names=x$Year[x$Year<=2014], col="#8bc2b6",
         ylim=ylim(x$Landings), ylab="Landings (1000 t)", main="Landings")
 box()
-invisible(dev.off())
+ido()
 
 tafpng("recruitment")
 barplot(x$Rec, names=x$Year, ylim=ylim(x$Rec), col="#424242",
         ylab="Recruitment (millions)", main="Recruitment (age 2)")
 box()
-invisible(dev.off())
+ido()
