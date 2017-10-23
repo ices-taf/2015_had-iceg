63bd5cb0c8dbe85f79a2b707930633b6e1bd1bd2
Arni Magnusson <arnima@hafro.is>
2017-10-23 13:46:43 +0200

    Use setwd("data") to shorten paths

--- data.R
+++ data.R
@@ -12,16 +12,18 @@ mkdir("data")
 url <- "https://raw.githubusercontent.com/ices-taf/ftp/master/nwwg/2015/had-iceg/raw/"
 
 ## Download data
-download(paste0(url,"catageysa.dat"), "data")  # later moved to input
-txt <- readLines("data/catageysa.dat", encoding="latin1")
+setwd("data")
+download(paste0(url,"catageysa.dat"))  # later moved to input
+txt <- readLines("catageysa.dat", encoding="latin1")
 
 ## Extract tables
 data <- extractInput(txt)
 
 ## Write tables to data directory
-write.taf(data$catage, "data/catage.csv")     # 1.2
-write.taf(data$smb, "data/smb.csv")           # 1.3
-write.taf(data$smh, "data/smh.csv")           # 1.4
-write.taf(data$wstock, "data/wstock.csv")     # 1.5
-write.taf(data$wcatch, "data/wcatch.csv")     # 1.6
-write.taf(data$maturity, "data/maturity.csv") # 1.7
+write.taf(data$catage, "catage.csv")     # 1.2
+write.taf(data$smb, "smb.csv")           # 1.3
+write.taf(data$smh, "smh.csv")           # 1.4
+write.taf(data$wstock, "wstock.csv")     # 1.5
+write.taf(data$wcatch, "wcatch.csv")     # 1.6
+write.taf(data$maturity, "maturity.csv") # 1.7
+setwd("..")
