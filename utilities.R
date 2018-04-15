a052e17dea0e677e95044a10acab616718980f2e
Arni Magnusson <arnima@hafro.is>
2018-04-15 07:57:58 +0200

    80 columns

--- utilities.R
+++ utilities.R
@@ -16,44 +16,57 @@ extractInput <- function(txt)
 
   ## 1  Catch at age
   catage <- data.frame(Year=firstyear:lastyear,
-                       read.table(text=txt, skip=grep("Landa.*ur afli",txt), nrows=nyrs))
+                       read.table(text=txt, skip=grep("Landa.*ur afli",txt),
+                                  nrows=nyrs))
   names(catage) <- c("Year", a1:nages)
 
   ## 2  Catch weights
   wcatch <- data.frame(Year=firstdatayear:(lastdatayear+4),
-                       read.table(text=txt, skip=grep("Weights in catch",txt), nrows=ndatayears+4))
+                       read.table(text=txt, skip=grep("Weights in catch",txt),
+                                  nrows=ndatayears+4))
   names(wcatch) <- c("Year", a1:nages)
 
   ## 3  Maturity
   maturity <- data.frame(Year=firstdatayear:(lastdatayear+4),
-                         read.table(text=txt, skip=grep("sexual maturity",txt), nrows=ndatayears+4))
+                         read.table(text=txt, skip=grep("sexual maturity",txt),
+                                    nrows=ndatayears+4))
   names(maturity) <- c("Year", a1:nages)
 
   ## 4  Stock weights
   wstock <- data.frame(firstdatayear:(lastdatayear+4),
-                       read.table(text=txt, skip=grep("Stockweights",txt), nrows=ndatayears+4))
+                       read.table(text=txt, skip=grep("Stockweights",txt),
+                                  nrows=ndatayears+4))
   names(wstock) <- c("Year", 1:nages)
 
   ## 5  Spring survey (SMB)
-  surveyfirstyear <- scan(text=txt, skip=grep("Survey data",txt)[1], nlines=1, quiet=TRUE)[1]
-  surveylastyear <- scan(text=txt, skip=grep("Survey data",txt)[1], nlines=1, quiet=TRUE)[2]
-  surveyfirstage <- scan(text=txt, skip=grep("ages for survey",txt)[1], nlines=1, quiet=TRUE)[1]
-  surveylastage <- scan(text=txt, skip=grep("ages for survey",txt)[1], nlines=1, quiet=TRUE)[2]
+  surveyfirstyear <- scan(text=txt, skip=grep("Survey data",txt)[1],
+                          nlines=1, quiet=TRUE)[1]
+  surveylastyear <- scan(text=txt, skip=grep("Survey data",txt)[1],
+                         nlines=1, quiet=TRUE)[2]
+  surveyfirstage <- scan(text=txt, skip=grep("ages for survey",txt)[1],
+                         nlines=1, quiet=TRUE)[1]
+  surveylastage <- scan(text=txt, skip=grep("ages for survey",txt)[1],
+                        nlines=1, quiet=TRUE)[2]
   smb <- data.frame(Year=surveyfirstyear:surveylastyear,
                     read.table(text=txt, skip=grep("#data",txt)[1],
                                nrows=surveylastyear-surveyfirstyear+1))
   names(smb) <- c("Year", surveyfirstage:surveylastage)
 
   ## 6  Autumn survey (SMH)
-  surveyfirstyear2 <- scan(text=txt, skip=grep("Survey data",txt)[2], nlines=1, quiet=TRUE)[1]
-  surveylastyear2 <- scan(text=txt, skip=grep("Survey data",txt)[2], nlines=1, quiet=TRUE)[2]
-  surveyfirstage2 <- scan(text=txt, skip=grep("ages for survey",txt)[2], nlines=1, quiet=TRUE)[1]
-  surveylastage2 <- scan(text=txt, skip=grep("ages for survey",txt)[2], nlines=1, quiet=TRUE)[2]
+  surveyfirstyear2 <- scan(text=txt, skip=grep("Survey data",txt)[2],
+                           nlines=1, quiet=TRUE)[1]
+  surveylastyear2 <- scan(text=txt, skip=grep("Survey data",txt)[2],
+                          nlines=1, quiet=TRUE)[2]
+  surveyfirstage2 <- scan(text=txt, skip=grep("ages for survey",txt)[2],
+                          nlines=1, quiet=TRUE)[1]
+  surveylastage2 <- scan(text=txt, skip=grep("ages for survey",txt)[2],
+                         nlines=1, quiet=TRUE)[2]
   smh <- data.frame(Year=surveyfirstyear2:surveylastyear2,
                     read.table(text=txt, skip=grep("#data",txt)[2],
                                nrows=surveylastyear2-surveyfirstyear2+1))
   names(smh) <- c("Year", surveyfirstage2:surveylastage2)
   smh[smh<0] <- NA
 
-  list(catage=catage, wcatch=wcatch, maturity=maturity, wstock=wstock, smb=smb, smh=smh)
+  list(catage=catage, wcatch=wcatch, maturity=maturity,
+       wstock=wstock, smb=smb, smh=smh)
 }
