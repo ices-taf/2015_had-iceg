8a5351466faa15dcdb99792caff4f4d40d1e23a0
Arni Magnusson <arnima@hafro.is>
2018-11-27 22:56:15 +0000

    Read from bootstrap folder, use os.unix() and taf.unzip()

--- model.R
+++ model.R
@@ -1,6 +1,6 @@
 ## Run analysis, write model results
 
-## Before: catageysa (begin/initial/model), catageysa.dat (input)
+## Before: catageysa (bootstrap/software), catageysa.dat (bootstrap/data)
 ## After:  resultsbyyear, resultsbyyearandage (model)
 
 library(icesTAF)
@@ -8,11 +8,11 @@ library(icesTAF)
 mkdir("model")
 
 ## Get model executable
-catageysa <- if(.Platform$OS.type == "unix") "catageysa" else "catageysa.exe"
-cp(file.path("begin/initial/model",catageysa), "model")
+exefile <- if(os.unix()) "catageysa" else "catageysa.exe"
+taf.unzip("bootstrap/software/catageysa.zip", files=exefile, exdir="model")
 
 ## Get model input file
-cp("input/catageysa.dat", "model")
+cp("bootstrap/data/catageysa.dat", "model")
 
 ## Run model
 setwd("model")
