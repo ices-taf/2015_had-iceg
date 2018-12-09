8fde59a9dfb31afd2c3ccf2ea1a5e54aa83dd2cd
Arni Magnusson <arnima@hafro.is>
2018-12-09 13:45:12 +0100

    icesTAF 2.0-0 notation: taf.png instead of tafpng

--- report_plots.R
+++ report_plots.R
@@ -13,12 +13,12 @@ x <- summary[summary$Year<=2016,]
 x <- div(x, c("Rec","B3plus","SSB","RefB","Landings"))
 
 ## Plots
-tafpng("biomass")
-plot(SSB ~ Year, x, type="l", lwd=2, ylim=lim(x$SSB), yaxs="i",
+taf.png("biomass")
+plot(SSB~Year, x, type="l", lwd=2, ylim=lim(x$SSB), yaxs="i",
      ylab="SSB (1000 t)", main="Biomass")
 dev.off()
 
-tafpng("harvest_rate")
-plot(HR ~ Year, x, subset=Year<=2014, type="l", lwd=2, ylim=lim(x$HR),
+taf.png("harvest_rate")
+plot(HR~Year, x, subset=Year<=2014, type="l", lwd=2, ylim=lim(x$HR),
      yaxs="i", ylab="Harvest rate", main="Harvest rate")
 dev.off()
