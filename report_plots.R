cf47a652cdeb2c57a306b224c0a0d2714fd3235a
Colin Millar <colinpmillar@gmail.com>
2018-12-09 09:52:21 +0100

    spaces in formula (testing githooks)

--- report_plots.R
+++ report_plots.R
@@ -14,11 +14,11 @@ x <- div(x, c("Rec","B3plus","SSB","RefB","Landings"))
 
 ## Plots
 tafpng("biomass")
-plot(SSB~Year, x, type="l", lwd=2, ylim=lim(x$SSB), yaxs="i",
+plot(SSB ~ Year, x, type="l", lwd=2, ylim=lim(x$SSB), yaxs="i",
      ylab="SSB (1000 t)", main="Biomass")
 dev.off()
 
 tafpng("harvest_rate")
-plot(HR~Year, x, subset=Year<=2014, type="l", lwd=2, ylim=lim(x$HR),
+plot(HR ~ Year, x, subset=Year<=2014, type="l", lwd=2, ylim=lim(x$HR),
      yaxs="i", ylab="Harvest rate", main="Harvest rate")
 dev.off()
